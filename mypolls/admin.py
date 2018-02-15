43331a0b32fb54cf5f41c3fd7688a89d38f63ee4
TaylorDarneille <tmdarneille@dons.usfca.edu>
2018-02-14 16:11:28 -0800

    customized admin

--- mypolls/admin.py
+++ mypolls/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
+# Change the title of the admin panel
+admin.AdminSite.site_header = 'My Super Awesome Admin Panel!'
+
+# Change the order of the entries
+# class QuestionAdmin(admin.ModelAdmin):
+# 	fields = ['pub_date', 'question_text']
+
+# Note - you can use StackedInline instead of TabularInline
+class ChoiceInLine(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+# Make fieldsets
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Question Information', {'fields': ['question_text']}),
+		('Date and Time Stuff', {'fields': ['pub_date']})
+	]
+	inlines = [ChoiceInLine]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- mypolls/models.py
+++ mypolls/models.py
@@ -12,6 +12,9 @@ class Question(models.Model):
 	def was_published_recently(self):
 		return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
 
+	was_published_recently.short_description = 'Published within the past 24 hrs.'
+	was_published_recently.boolean = True
+
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=100)
